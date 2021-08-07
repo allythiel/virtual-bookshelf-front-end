4c7ba36dad2b19726e62530ec832c04bd89116a8
allythiel <allythiel@gmail.com>
2021-08-07 11:45:08 -0500

    Add Book Description

--- src/components/activeBook/activeBook.js
+++ src/components/activeBook/activeBook.js
@@ -6,7 +6,8 @@ const ActiveBook = (props) => {
         <div className='active-book'>
             {props.currentBook ? <iframe id="bookviewer" title="activebook" width="600px" height="500px" src={`https://www.google.com/books/jsapi.js${props.currentBook.id.bookId}`}></iframe> : 'Loading . . .'}
             {props.currentBook ? props.currentBook.volumeInfo ? <div><h2>{props.currentBook.volumeInfo.title}</h2> </div> : '' : '' }
-            {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>Average Rating: </h3><h3>{props.currentBook.volumeInfo.averageRating}</h3> </div> : '' : '' }
+            {props.currentBook ? props.currentBook.volumeInfo ? <div><p>{props.currentBook.volumeInfo.description}</p> </div> : '' : '' }
+            {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>Average Rating: {props.currentBook.volumeInfo.averageRating}</h3> </div> : '' : '' }
         </div>
     )
 }
