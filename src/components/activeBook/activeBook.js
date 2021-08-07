ee8d205a71f9aa4cc6619870b69ff80aa9256d50
allythiel <allythiel@gmail.com>
2021-08-07 12:53:54 -0500

    Add Author with Book Title

--- src/components/activeBook/activeBook.js
+++ src/components/activeBook/activeBook.js
@@ -6,7 +6,7 @@ const ActiveBook = (props) => {
         <div className='active-book'>
             <div><img src={props.currentBook.volumeInfo.imageLinks.thumbnail} alt="" /></div>
             {/* {props.currentBook ? <iframe id="bookviewer" title="activebook" width="600px" height="500px" src={`https://www.google.com/books/jsapi.js${props.currentBook.id.bookId}`}></iframe> : 'Loading . . .'} */}
-            {props.currentBook ? props.currentBook.volumeInfo ? <div><h2>{props.currentBook.volumeInfo.title}</h2> </div> : '' : '' }
+            {props.currentBook ? props.currentBook.volumeInfo ? <div><h2>{props.currentBook.volumeInfo.title} - {props.currentBook.volumeInfo.authors}</h2></div> : '' : '' }
             {props.currentBook ? props.currentBook.volumeInfo ? <div><p>{props.currentBook.volumeInfo.description}</p> </div> : '' : '' }
             {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>Average Rating: {props.currentBook.volumeInfo.averageRating} /5 </h3> </div> : '' : '' }
         </div>
