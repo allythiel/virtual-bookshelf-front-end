af4beedc31c8d97612ceffd5dd7a3c505caa998d
allythiel <allythiel@gmail.com>
2021-08-07 11:41:28 -0500

    Add Average Rating

--- src/components/activeBook/activeBook.js
+++ src/components/activeBook/activeBook.js
@@ -5,7 +5,8 @@ const ActiveBook = (props) => {
     return (
         <div className='active-book'>
             {props.currentBook ? <iframe id="bookviewer" title="activebook" width="600px" height="500px" src={`https://www.google.com/books/jsapi.js${props.currentBook.id.bookId}`}></iframe> : 'Loading . . .'}
-            {props.currentBook ? props.currentBook.volumeInfo ? <div><h2>{props.currentBook.volumeInfo.title}</h2> <h3>{props.currentBook.volumeInfo.averageRating}</h3></div> : '' : '' }
+            {props.currentBook ? props.currentBook.volumeInfo ? <div><h2>{props.currentBook.volumeInfo.title}</h2> </div> : '' : '' }
+            {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>Average Rating: </h3><h3>{props.currentBook.volumeInfo.averageRating}</h3> </div> : '' : '' }
         </div>
     )
 }
