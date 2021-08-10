97cddc65f6172c33288b4830e312e07c909bdb40
allythiel <allythiel@gmail.com>
2021-08-09 21:26:13 -0500

    Add Files for Comments

---
+++ src/components/comments/createComment/createComment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './createComment.css';
+
+const CreateComment = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CreateComment
---
+++ src/components/comments/listComments/listComments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './listComments.css';
+
+const ListComments = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ListComments
