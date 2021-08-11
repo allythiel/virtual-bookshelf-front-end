088b4e084d66f10017b72e9246cf961215d4325a
allythiel <allythiel@gmail.com>
2021-08-10 20:45:38 -0500

    Uninstall Material UI

--- src/components/messageBar/messageBar.js
+++ src/components/messageBar/messageBar.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { CancelRounded } from '@material-ui/icons';
 import './messageBar.css'
 
 const MessageBar = (props) => {
    return (
       <div className='message-bar'>
-         <div className='message-text'>{props.messageText}</div><div className='cancel-icon' onClick={props.handleCloseMessageBar}><CancelRounded fontSize='large' /></div>
+         <div className='message-text'>{props.messageText}</div><div className='cancel-icon' onClick={props.handleCloseMessageBar}></div>
       </div>
    )
 }
