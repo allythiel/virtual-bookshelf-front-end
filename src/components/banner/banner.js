2bdf36997f57f6370d1480c6fe8a3b97f30ab035
allythiel <allythiel@gmail.com>
2021-08-11 19:02:31 -0500

    Add Logout Functionality

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,20 +2,18 @@ import React from 'react';
 import './banner.css';
 import logo from '../../images/bookshelf-logo.png';
 import loginAvatar from '../../images/login-placeholder.png';
+import BannerActionBar from '../bannerActionBar/bannerActionBar';
 
 const Banner = (props) => {
    return (
       <div className='banner'>
          <div>
-            <div><img src={logo} alt="" onClick={()=>{props.setLoggedInUser(props.loggedInUser)}} /></div>
+            <div><img src={logo} alt="" onClick={()=>{props.setLoggedInUser(props.loggedinUser)}} /></div>
             <div><h1>Virtual Bookshelf</h1></div>
          </div>
          <div>
       <div className="banner-action-bar">
-         {/* <div className="user-info">Hi, {props.loggedInUser.name.split(' ')[0]}
-         </div> */}
-         <div className="login"><img onClick={props.handleLogin} src={loginAvatar}
-            alt="" /></div>
+      {(props.loggedIn && props.loggedInUser) && <BannerActionBar loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} handleLogin={props.handleLogin} loggedInUser={props.loggedInUser} /> }
          </div>            
          </div>
       </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ import './header.css';
 const Header = (props) => {
    return (
       <div className='header'>
-         <Banner loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} handleLogin={props.handleLogin} loggedInUser={props.loggedInUser} />
+         <Banner loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} handleLogin={props.handleLogin} loggedInUser={props.loggedInUser} setLoggedInUser={props.SetLoggedInUser} />
       </div>
    )
 }
