0e687b180af2720b160a639e130e1253d0f5d29b
allythiel <allythiel@gmail.com>
2021-08-21 11:21:05 -0500

    Add Material UI. Add Button Framework for Like/Dislike

--- src/components/activeBook/bookRatings/bookRatings.js
+++ src/components/activeBook/bookRatings/bookRatings.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { ThumbDownAlt } from '@material-ui/icons';
+import { ThumbUpAlt } from '@material-ui/icons';
+
 
 const BookRatings = (props) => {
+    console.log('bookRatings', props.currentBook)
     return (
-        <div>
+        <div className='book-ratings'>
+            <button onClick={() => {props.handleLike}}><ThumbUpAlt /></button>
+            <button onClick={() => {props.handleDislike}}><ThumbDownAlt /></button>
             
         </div>
     )
