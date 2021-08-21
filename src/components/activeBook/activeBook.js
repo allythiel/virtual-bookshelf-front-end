1796c22414abcb85a0dcc28effaa3306132f9182
allythiel <allythiel@gmail.com>
2021-08-21 12:13:38 -0500

    Update Active Book Layout and Styling

--- src/components/activeBook/activeBook.js
+++ src/components/activeBook/activeBook.js
@@ -10,14 +10,13 @@ const ActiveBook = (props) => {
             <div><img src={props.currentBook.volumeInfo.imageLinks.thumbnail} alt="" /></div>
                 <div className='book-info'> 
                 {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>{props.currentBook.volumeInfo.title} - {props.currentBook.volumeInfo.authors}</h3></div> : '' : '' }
-                {props.currentBook ? props.currentBook.volumeInfo ? <div><p>{props.currentBook.volumeInfo.description}</p> </div> : '' : '' }
+                {props.currentBook ? props.currentBook.volumeInfo ? <div><p>{props.currentBook.volumeInfo.description}</p> </div> : '' : '' } </div>
             <div className='book-ratings'>
-                {props.currentBook ? props.currentBook.volumeInfo ? <div><h4>Average Rating: {props.currentBook.volumeInfo.averageRating} </h4> </div> : '' : '' }
+                {props.currentBook ? props.currentBook.volumeInfo ? <div><h4>Average Rating: {props.currentBook.volumeInfo.averageRating} </h4> </div> : '' : '' }  
                 {props.currentBook ? <BookRatings users={props.users} currentBook={props.currentBook} setCurrentBook={props.setCurrentBook}/> : '' }
         <div className='view-comments'>
             {props.currentBook ? <CreateComment users={props.users} handleNewCommentChange={props.handleNewCommentChange} handleNewCommentSubmit={props.handleNewCommentSubmit} newComment = {props.newComment} setNewComment={props.setNewComment} comments={props.comments} setComments={props.setComments}/>  : ''}
             {props.currentBook ? <ListComments users={props.users} newComment={props.newComment} setNewComment={props.setNewComment} comments={props.comments} setComments={props.setComments} commentCount={props.commentCount} setCommentCount={props.setCommentCount} /> : '' }
-        </div>
         </div>
         </div>
         </div>
