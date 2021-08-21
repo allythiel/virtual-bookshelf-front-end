dcc64931fe7360e821a120dce590055e4386c24e
allythiel <allythiel@gmail.com>
2021-08-21 18:14:17 -0500

    Update Flex Format for Active Book. Add Border

--- src/components/activeBook/activeBook.js
+++ src/components/activeBook/activeBook.js
@@ -7,10 +7,11 @@ import BookRatings from './bookRatings/bookRatings';
 const ActiveBook = (props) => {
     return (
         <div className='active-book'>
-            <div><img src={props.currentBook.volumeInfo.imageLinks.thumbnail} alt="" /></div>
-                <div className='book-info'> 
-                {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>{props.currentBook.volumeInfo.title} - {props.currentBook.volumeInfo.authors}</h3></div> : '' : '' }
-                {props.currentBook ? props.currentBook.volumeInfo ? <div><p>{props.currentBook.volumeInfo.description}</p> </div> : '' : '' } </div>
+                <div className='book-image'> 
+                <div><img src={props.currentBook.volumeInfo.imageLinks.thumbnail} alt="" /></div>
+                    <div className='book-info'>
+                    {props.currentBook ? props.currentBook.volumeInfo ? <div><h3>{props.currentBook.volumeInfo.title} - {props.currentBook.volumeInfo.authors}</h3></div> : '' : '' }
+                    {props.currentBook ? props.currentBook.volumeInfo ? <div><p>{props.currentBook.volumeInfo.description}</p> </div> : '' : '' } </div></div>
             <div className='book-ratings'>
                 {props.currentBook ? props.currentBook.volumeInfo ? <div><h4>Average Rating: {props.currentBook.volumeInfo.averageRating} </h4> </div> : '' : '' }  
                 {props.currentBook ? <BookRatings users={props.users} currentBook={props.currentBook} setCurrentBook={props.setCurrentBook}/> : '' }
