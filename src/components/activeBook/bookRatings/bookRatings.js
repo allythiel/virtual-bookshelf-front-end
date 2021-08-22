6b500fad09e6b1396c427bbcc430660a1164e5f4
allythiel <allythiel@gmail.com>
2021-08-21 19:33:07 -0500

    Install Bootstrap Icons and Update Material UI

--- src/components/activeBook/bookRatings/bookRatings.js
+++ src/components/activeBook/bookRatings/bookRatings.js
@@ -3,6 +3,7 @@ import { ThumbDownAlt } from '@material-ui/icons';
 import { ThumbUpAlt } from '@material-ui/icons';
 
 
+
 const BookRatings = (props) => {
     console.log('bookRatings', props.currentBook)
     return (
